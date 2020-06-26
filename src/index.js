8178c61504e5dc82829511fdcc1ac612e838f7cf
santiagt <santiagotorresredecilla@gmail.com>
2020-06-27 00:11:05 +0300

    getting started

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import "./styles.css";
-lat boardArr = ["","","O","","","","","",""];
+let boardArr = ["","","O","","","","","",""];
 
-const printBoard = () => {
-  let board = "";
-  let colc = 0;
-  for (i=0; i<boardArr.length; i++) {
-    if (colc === 0) board += "<tr>";
-    board += "<td>" + boardArr[i] + "</td>";
-    if (colc === 2)  {
-      board += "</tr>";
-      colc = 0;
-  } else {
-    colc++;
-  }
-  }
-};
-printBoard();
+let board = "<table border=1>";
+let colc = 0;
+for (let i=0; i<boardArr.length; i++) {
+  if (colc === 0) board += "<tr>";
+  board += "<td width=75px height=75px> <p style='text-align:center; font-size:30px;' > " + boardArr[i] + "</p> </td>";
+  if (colc === 2)  {
+    board += "</tr>";
+    colc = 0;
+} else {
+  colc++;
+}
+}
+board += "</table>";
+
 document.getElementById("board").innerHTML = board;
