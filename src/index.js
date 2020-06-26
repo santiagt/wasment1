b0e882096726f265c4e9cd94297d42440ea06ddd
santiagt <santiagotorresredecilla@gmail.com>
2020-06-27 01:44:42 +0300

    getting started

--- src/index.js
+++ src/index.js
@@ -4,17 +4,27 @@ for (let x=0; x<5; x++) {
   boardArr.push(["","","","",""]);
 }
 
-let board = "<table border=1>";
-let id = "";
-for (let i=0; i<boardArr.length; i++) {
-  board += "<tr>";
-  for (let j=0; j<bardArr[i].length; j++) {
-    id = i + j; 
-    board += "<td width=50px height=50px> <p id=('id') style='text-align:center; font-size:25px;' > " + boardArr[i] + "</p> </td>";
-    console.log(board);
-  }
-  board +="</tr>";
-}
-board += "</table>";
+document.addEventListener('click', (event) => {
+  event.preventDefault();
+  console.log(event.target);
+});
 
-document.getElementById("board").innerHTML = board;
+const printBoard = () => {
+  let board = "<table border=1>";
+
+  for (let i=0; i<boardArr.length; i++) {
+    board += "<tr>";
+    for (let j=0; j<boardArr[i].length; j++) {
+      let id = i +","+ j; 
+      board += `<td id=${id} width=50px height=50px> <p style='text-align:center; font-size:25px;' > ` 
+                + boardArr[i][j] + "</p> </td>";
+    }
+    board +="</tr>";
+  }
+  board += "</table>";
+  console.log(board);
+  return board;
+};
+
+
+document.getElementById("board").innerHTML = printBoard();
