ede58b4eb0961659523decf0cf110229632cd8db
santiagt <santiagotorresredecilla@gmail.com>
2020-06-27 00:54:07 +0300

    getting started

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ for (let x=0; x<5; x++) {
 }
 
 let board = "<table border=1>";
+let id = "";
 for (let i=0; i<boardArr.length; i++) {
   board += "<tr>";
   for (let j=0; j<bardArr[i].length; j++) {
-    board += "<td width=50px height=50px> <p style='text-align:center; font-size:25px;' > " + boardArr[i] + "</p> </td>";
+    id = i + j; 
+    board += "<td width=50px height=50px> <p id=('id') style='text-align:center; font-size:25px;' > " + boardArr[i] + "</p> </td>";
+    console.log(board);
   }
   board +="</tr>";
 }
